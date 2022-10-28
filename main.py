5e5ecf97147af69eae3a1aa56da32cdad91dd114
Gianmarco Wyss <gianmarco_wyss@sluz.ch>
2022-10-28 14:39:33 +0200

    vol2

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def info() -> typing.Dict:
     print("INFO")
 
     return {
-        "apiversion": "1",
+        "apiversion": "2",
         "author": "Giani-Wyss",  # TODO: Your Battlesnake Username
         "color": "#1365C1",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "head": "smile",  # TODO: Choose head
+        "tail": "hook",  # TODO: Choose tail
     }
 
 
@@ -63,8 +63,20 @@ def move(game_state: typing.Dict) -> typing.Dict:
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+
+    if my_head["x"] == board_width["min"]:  # Bound is left of head, don't move left
+        is_move_safe["left"] = False
+
+    elif my_head["x"] == board_width["max"]:  # Bound is right of head, don't move right
+        is_move_safe["right"] = False
+
+    elif my_head["y"] < board_height["min"]:  # Bound is below head, don't move down
+        is_move_safe["down"] = False
+
+    elif my_head["y"] < board_height["max"]:  # Bound is above head, don't move up
+        is_move_safe["up"] = False
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
