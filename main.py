a9a02b6efc054ff09fdad4b0eee33741b359e88e
Gianmarco Wyss <gianmarco_wyss@sluz.ch>
2022-10-29 15:26:18 +0200

    vol3.8

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
     lowestop = []
     distancetoop = 99
     if snakes_copy == []:
-        snakes_copy == snake
+        snakes_copy['head'] = nearestfood
     for f_op in snakes_copy:
         fight_op = f_op['head']
         tempdistancetoop = abs(fight_op["x"] - my_head["x"]) + abs(fight_op["y"] - my_head["y"])
