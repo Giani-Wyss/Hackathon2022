ddedd8651239a44103efea7024994369feb56cc2
Gianmarco Wyss <gianmarco_wyss@sluz.ch>
2022-10-29 06:43:06 +0200

    vol2.82

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
             op_next_move.append(op_next_move_right)
             op_next_move.append(op_next_move_down)
             op_next_move.append(op_next_move_up)
-            print(op_next_move)
+            print(op_next_move['x'], ['y'])
 
             for Op_move in op_next_move:
                 if Op_move == next_move_left:
